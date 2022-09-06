d74abb1a5b39cd9f328ae00be95843eee6ff199c
DiegoAndresMarmota <diegoechalarp@hotmail.com>
2022-09-06 19:08:05 -0300

    Actualización de vistas

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import express from "express";
 
-const app = express();
+const app = express()
 
-app.get("/", (req, res) => res.send("Hello there"));
+//Se extrae de la carpeta de las vistas a través de:
+app.set('views', )
+
+//Activar caracteristica de View Engine
+app.set('view engine', 'ejs')
+
+//Renderizar en el front, un archivo html, a traves de la ruta .ejs
+app.get("/", (req, res) => res.render('index'));
 
 app.listen(3000);
 
